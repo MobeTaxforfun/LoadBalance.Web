4630267bdae4ef0052bc769d6a7152d084934cc0
MobeWu <mobetax81072@gmail.com>
2022-05-15 22:25:38 +0800

    更新 新增:使用docker-compose啟動 MSSQL 與三個 Web Container 新增:.env 使用環境參數的方式設定 docker-compose.yml 的參數 新增:LoadBalance.Web 中使用 Migration 與 SeedData 還原資料庫 新增:以參數化的方式設定 LoadBalance.Web.csproj 中的 Db connect string 新增:docker-compose下的Data資料料夾 用來持久化 各個Container中的資料

---
+++ Src/LoadBalance/LoadBalance.Web/Models/Migration/MigrationDb.cs
@@ -0,0 +1,21 @@
+﻿using LoadBalance.Web.Models.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoadBalance.Web.Models.Migration
+{
+    public static class MigrationDb
+    {
+        public static void MigrationTestDb(this IServiceProvider provider)
+        {
+            using (var scope = provider.CreateScope())
+            {
+                var dbcontext = scope.ServiceProvider.GetRequiredService<LoadBalanceDBContext>();
+                if (!dbcontext.Database.CanConnect())
+                {
+                    dbcontext.Database.Migrate();
+                    dbcontext.Database.EnsureCreated();
+                }
+            }
+        }
+    }
+}
---
+++ Src/LoadBalance/LoadBalance.Web/Models/SeedData/SeedData.cs
@@ -0,0 +1,30 @@
+﻿using LoadBalance.Web.Models.Context;
+
+namespace LoadBalance.Web.Models.SeedData
+{
+    public static class SeedData
+    {
+        public static void EnsureSeedData(this IServiceProvider provider)
+        {
+            using (var scope = provider.CreateScope())
+            {
+                var dbcontext = scope.ServiceProvider.GetRequiredService<LoadBalanceDBContext>();
+                if (!dbcontext.FreeRapid.Any())
+                {
+                    dbcontext.FreeRapid.Add(new Entity.FreeRapid
+                    {
+                        Id = 1,
+                        City = "臺北市",
+                        Town = "士林區",
+                        RapidName = "粱耳鼻喉科診所",
+                        Address = "臺北市士林區社正路12-1號",
+                        PhoneNumber = "02-28168456",
+                        Lat = 121.509303,
+                        Long = 25.08855
+                    });
+                    dbcontext.SaveChanges();
+                }
+            }
+        }
+    }
+}
